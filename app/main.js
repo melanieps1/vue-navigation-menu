27f386802025bb18d670c2023561bea1aea18609
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-10-03 11:14:49 -0400

    Added message with Vue

---
+++ app/main.js
@@ -0,0 +1,17 @@
+// Create a Vue instance
+var demo = new Vue({
+
+	// A DOM element to mount our initial view model, linked to an id in index.html
+	el: '#menu',
+
+	// Define some data and properties
+	data: {
+		message: 'Please click a menu item'
+	},
+
+	// Define some functions (functions go in methods)
+	methods: {
+
+	}
+
+});
